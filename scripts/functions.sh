3e379d9fa8aa49a185e55ba06ab16f889ae83571
Moul <moul@moul.re>
2018-11-29 17:03:39 +0100

    [mod] #51: remove no longer neccessary `sudo`

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -15,19 +15,19 @@ url="${git_repo}${middle_url}${deb}"
 # Retrieve debian package and install it
 wget -nc --quiet $url -P /tmp
 deb_path="/tmp/$deb"
-sudo dpkg -i $deb_path > /dev/null
-sudo rm -f $deb_path
+dpkg -i $deb_path > /dev/null
+rm -f $deb_path
 }
 
 CONFIGURE_DUNITER () {
-	sudo duniter config --ipv4 127.0.0.1 --port $port --remoteh $domain --remotep 80 --noupnp
-	sudo duniter config --addep "BMAS $domain 443"
-	sudo duniter config --ws2p-host 127.0.0.1 --ws2p-port 20901 --ws2p-remote-host $domain --ws2p-remote-port 443 --ws2p-noupnp
+	duniter config --ipv4 127.0.0.1 --port $port --remoteh $domain --remotep 80 --noupnp
+	duniter config --addep "BMAS $domain 443"
+	duniter config --ws2p-host 127.0.0.1 --ws2p-port 20901 --ws2p-remote-host $domain --ws2p-remote-port 443 --ws2p-noupnp
 }
 
 CONFIG_SSOWAT () {
 # Add admin to the allowed users
-sudo yunohost app addaccess $app -u $admin
+yunohost app addaccess $app -u $admin
 
 # Protect senstive sub-routes
 ynh_app_setting_set "$app" protected_uris "/webui","/webmin"
@@ -42,19 +42,19 @@ ynh_app_setting_set "$app" redirected_urls "{'$domain/':'$domain/webui'}"
 CONFIG_NGINX () {
 # Configure Nginx
 nginx_conf="../conf/nginx.conf"
-sudo sed -i "s@YNH_EXAMPLE_PORT@$port@" $nginx_conf
-sudo sed -i "s@YNH_EXAMPLE_DOMAIN@$domain@" $nginx_conf
-sudo cp $nginx_conf /etc/nginx/conf.d/$domain.d/$app.conf
-sudo service nginx reload
+sed -i "s@YNH_EXAMPLE_PORT@$port@" $nginx_conf
+sed -i "s@YNH_EXAMPLE_DOMAIN@$domain@" $nginx_conf
+cp $nginx_conf /etc/nginx/conf.d/$domain.d/$app.conf
+service nginx reload
 }
 
 REMOVE_DUNITER () {
 # Stop duniter daemon if running
-sudo duniter status
+duniter status
 if [ `echo "$?"` == 0 ]; then
-    sudo duniter stop
+    duniter stop
 fi
 
 # Remove Duniter package
-sudo dpkg -r duniter
+dpkg -r duniter
 }
