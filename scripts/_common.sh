6acb783f35ddd65f79fb3d3cf064d88c5e9147df
frju365 <abld@abld.info>
2018-11-30 12:26:36 +0100

    [fix] little Refactoring to use ynh_helper

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-INSTALL_DUNITER_DEBIAN_PACKAGE () {
+install_duniter_debian_package() {
 	version="v1.6.25"
 	git_repo="https://git.duniter.org/nodes/typescript/duniter/"
 	if [ $arch == "x64" ]; then
@@ -18,13 +18,13 @@ INSTALL_DUNITER_DEBIAN_PACKAGE () {
 	rm -f $deb_path
 }
 
-CONFIGURE_DUNITER () {
+config_duniter() {
 	duniter config --ipv4 127.0.0.1 --port $port --remoteh $domain --remotep 80 --noupnp
 	duniter config --addep "BMAS $domain 443"
 	duniter config --ws2p-host 127.0.0.1 --ws2p-port 20901 --ws2p-remote-host $domain --ws2p-remote-port 443 --ws2p-noupnp
 }
 
-CONFIG_SSOWAT () {
+config_ssowat() {
 	# Add admin to the allowed users
 	yunohost app addaccess $app -u $admin
 
@@ -38,15 +38,7 @@ CONFIG_SSOWAT () {
 	ynh_app_setting_set "$app" redirected_urls "{'$domain/':'$domain/webui'}"
 }
 
-CONFIG_NGINX () {
-	nginx_conf="../conf/nginx.conf"
-	sed -i "s@YNH_EXAMPLE_PORT@$port@" $nginx_conf
-	sed -i "s@YNH_EXAMPLE_DOMAIN@$domain@" $nginx_conf
-	cp $nginx_conf /etc/nginx/conf.d/$domain.d/$app.conf
-	service nginx reload
-}
-
-REMOVE_DUNITER () {
+remove_duniter() {
 	# Stop duniter daemon if running
 	duniter status
 	if [ `echo "$?"` == 0 ]; then
