f9c9c5c5fd7ef3d17e5a78ecb150a605c3c396de
Moul <moul@moul.re>
2022-05-05 19:27:28 +0200

    [enh] Add BMA endpoint definition with /bma path
    I thought there were an issue to apply because of the already
    existing and auto-generated BMAS endpoint
    In reality, it wasn’t applied because of the issue from previous commit:
    conf not applied in the right config directory
    
    Remove autogenerated endpoint

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,6 +14,8 @@ CONFIGURE_DUNITER() {
 	ynh_exec_as duniter duniter --home $datadir config --bma --ipv4 127.0.0.1 --port $port --remoteh $domain --remotep 443 --noupnp
 	ynh_exec_as duniter duniter --home $datadir config --ws2p-host 127.0.0.1 --ws2p-port 20901 --ws2p-remote-host $domain --ws2p-remote-port 443 --ws2p-remote-path "/ws2p" --ws2p-noupnp
 
+	# Add BMAS with path, remove auto-generated BMAS endpoint
+	ynh_exec_as duniter duniter --home $datadir config --addep "BMAS $domain 443 /bma" --remep "BMAS $domain 443"
 }
 
 #=================================================
