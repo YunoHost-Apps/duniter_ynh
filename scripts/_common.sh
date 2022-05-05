5d83c9c7f7c54ba53bcfe1bfb4e412b0870f01fb
Moul <moul@moul.re>
2022-05-05 19:25:36 +0200

    [fix] Fix home directory specification when applying config
    I thought specifying 'duniter'’s $HOME to
    /home/yunohost.app/duniter to be enough
    The config actually went into:
    /home/yunohost.app/duniter/.config/duniter_default/
    
    Specify '--home $datadir', so the configuration goes into
    /home/yunohost.app/duniter/duniter_default/
    
    Update documentation accordingly with \$HOME to make
    the already a long and relative complex command shorter

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,8 +11,8 @@ pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP
 # PERSONAL HELPERS
 #=================================================
 CONFIGURE_DUNITER() {
-	ynh_exec_as duniter duniter config --bma --ipv4 127.0.0.1 --port $port --remoteh $domain --remotep 443 --noupnp
-	ynh_exec_as duniter duniter config --ws2p-host 127.0.0.1 --ws2p-port 20901 --ws2p-remote-host $domain --ws2p-remote-port 443 --ws2p-remote-path "/ws2p" --ws2p-noupnp
+	ynh_exec_as duniter duniter --home $datadir config --bma --ipv4 127.0.0.1 --port $port --remoteh $domain --remotep 443 --noupnp
+	ynh_exec_as duniter duniter --home $datadir config --ws2p-host 127.0.0.1 --ws2p-port 20901 --ws2p-remote-host $domain --ws2p-remote-port 443 --ws2p-remote-path "/ws2p" --ws2p-noupnp
 
 }
 
