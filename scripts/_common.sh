f2bceb1eac76ce410293b76af5a4bf096924c45e
Félix Piédallu <felix@piedallu.me>
2024-02-21 15:19:12 +0100

    Rename ports

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,10 +10,10 @@
 
 CONFIGURE_DUNITER() {
     ynh_exec_as "duniter" duniter --home "$data_dir" config \
-        --bma --ipv4 127.0.0.1 --port $bma_local_port --remoteh $domain --remotep 443 --noupnp
+        --bma --ipv4 127.0.0.1 --port $port_bma_local --remoteh $domain --remotep 443 --noupnp
 
     ynh_exec_as "duniter" duniter --home "$data_dir" config \
-        --ws2p-host 127.0.0.1 --ws2p-port $ws2p_local_port \
+        --ws2p-host 127.0.0.1 --ws2p-port $port_ws2p_local \
         --ws2p-remote-host "$domain" --ws2p-remote-port 443 \
         --ws2p-remote-path "/ws2p" --ws2p-noupnp
 
