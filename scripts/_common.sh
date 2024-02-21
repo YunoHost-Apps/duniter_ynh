8ce31e51fc08a10f5b54b78e2ee814dab85d72d6
Félix Piédallu <felix@piedallu.me>
2024-02-21 18:01:03 +0100

    fix ports again

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,10 +10,10 @@
 
 CONFIGURE_DUNITER() {
     ynh_exec_as "duniter" duniter --home "$data_dir" config \
-        --bma --ipv4 127.0.0.1 --port "$local_port_bma" --remoteh $domain --remotep 443 --noupnp
+        --bma --ipv4 127.0.0.1 --port "$port_local_bma" --remoteh $domain --remotep 443 --noupnp
 
     ynh_exec_as "duniter" duniter --home "$data_dir" config \
-        --ws2p-host 127.0.0.1 --ws2p-port "$local_port_ws2p" \
+        --ws2p-host 127.0.0.1 --ws2p-port "$port_local_ws2p" \
         --ws2p-remote-host "$domain" --ws2p-remote-port 443 \
         --ws2p-remote-path "/ws2p" --ws2p-noupnp
 
