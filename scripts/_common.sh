af07d5b0132bb7e890220f2a492211775a3f13c4
Félix Piédallu <felix@piedallu.me>
2024-02-23 16:59:23 +0100

    Add warning, wipe data when moving to 1.8.7

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,18 +8,21 @@
 # PERSONAL HELPERS
 #=================================================
 
-CONFIGURE_DUNITER() {
-    ynh_exec_as "duniter" duniter --home "$data_dir" config \
-        --bma --ipv4 127.0.0.1 --port "$port_local_bma" --remoteh $domain --remotep 443 --noupnp
+run_duniter() {
+    ynh_exec_as "duniter" duniter --home "$data_dir" "$@"
+}
 
-    ynh_exec_as "duniter" duniter --home "$data_dir" config \
+
+CONFIGURE_DUNITER() {
+    run_duniter config --bma --ipv4 127.0.0.1 --port "$port_local_bma" --remoteh $domain --remotep 443 --noupnp
+
+    run_duniter config \
         --ws2p-host 127.0.0.1 --ws2p-port "$port_local_ws2p" \
         --ws2p-remote-host "$domain" --ws2p-remote-port 443 \
         --ws2p-remote-path "/ws2p" --ws2p-noupnp
 
     # Add BMAS with path, remove auto-generated BMAS endpoint
-    ynh_exec_as "duniter" duniter --home "$data_dir" config \
-        --addep "BMAS $domain 443 /bma" --remep "BMAS $domain 443"
+    run_duniter config --addep "BMAS $domain 443 /bma" --remep "BMAS $domain 443"
 }
 
 #=================================================
